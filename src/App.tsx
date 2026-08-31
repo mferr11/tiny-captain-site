b343c3f5babc865fd8d3867f3af9b37c0fe80ec8
mferr11 <mitchellferreira13@gmail.com>
2026-08-31 18:53:48 +1000

    Added small animation when changing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import ScrollToTop from './components/ScrollToTop'
+import './App.css'
 
 export default function App() {
+  const { pathname } = useLocation()
+
   return (
     <>
       <ScrollToTop />
       <Header />
-      <main>
+      <main key={pathname} className="page-transition">
         <Outlet />
       </main>
       <Footer />
